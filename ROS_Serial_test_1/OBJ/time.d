6d25cb07084ecbf67f77206ebeacba452322d69a
Xiaowei(Caesar) Lin <xiaowei.lin1997@gmail.com>
2020-10-22 16:26:12 +0800

    Serial test updated, working and the sync problem located

--- ROS_Serial_test_1/OBJ/time.d
+++
@@ -1,5 +0,0 @@
-..\obj\time.o: ..\RosLibs\time.cpp
-..\obj\time.o: ..\RosLibs\ros/time.h
-..\obj\time.o: ..\RosLibs\ros/duration.h
-..\obj\time.o: D:\MDK5\ARM\ARMCC\Bin\..\include\math.h
-..\obj\time.o: ..\MDK-ARM\round.h
